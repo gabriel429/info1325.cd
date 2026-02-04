ece79415e13bee40534b5ad7daa9e1f5698a4ba2
Gabriel Muya <igabrielmuya@gmail.com>
2026-02-04 16:50:42 +0100

    Load gmaps and map-active before main.js to fix GMaps not defined

--- pagesweb/footerPage.php
+++ pagesweb/footerPage.php
@@ -1,138 +1,278 @@
-<!-- Footer Area -->
-<footer id="footer" class="footer">
-    <!-- Footer Top -->
-    <div class="footer-top">
-        <div class="container">
-            <div class="row">
-
-                <div class="col-lg-3 col-md-6 col-12">
-                    <div class="single-footer">
-                        <h2>Suivez-nous</h2>
-                        <p>
-                            <img src="<?= IMG_DIR ?>logoMingenre03.png" alt="" width="100%" height="100" style="display:block;">
-                        </p>
-                        <!-- <p>Le Secrétariat National de la Résolution 1325 en RDC, est une structure du suivi et de la gestion quotidienne de la mise en œuvre de ladite Résolution sur l’ensemble du Pays.</p> -->
-                        <!-- Social -->
-                        <ul class="social">
-                            <li><a href="https://web.facebook.com/sn1325/" target="_blank"><i class="icofont-facebook"></i></a></li>
-                            <li><a href="https://www.linkedin.com/company/r1325rdc/" target="_blank"><i class="icofont-linkedin"></i></a></li>
-                            <li><a href="https://x.com/R1325RDC" target="_blank"><i class="icofont-twitter"></i></a></li>
-                            <li><a href="https://youtube.com/@resolution1325rdc?si=IWwQB3N7fc5RFUHD" target="_blank"><i class="icofont-youtube"></i></a></li>
-                        </ul>
-                        <!-- End Social -->
-                    </div>
-                </div>
-
-                <div class="col-lg-3 col-md-6 col-12">
-                    <div class="single-footer f-link">
-                        <h2>Liens utiles</h2>
-                        <div class="row">
-                            <div class="col-lg-6 col-md-6 col-12">
-                                <ul>
-                                    <li><a href="https://genre.gouv.cd/" target="_blank"><i class="fa fa-caret-right" aria-hidden="true"></i>MGFE</a></li>
-                                    <li><a href="https://www.unwomen.org/fr" target="_blank"><i class="fa fa-caret-right" aria-hidden="true"></i>ONUFEMMES</a></li>
-                                    <li><a href="https://www.norway.no/fr/democratic-republic-of-congo/" target="_blank"><i class="fa fa-caret-right" aria-hidden="true"></i>AMBASSADE NORVEGE</a></li>
-                                    <li><a href="#" target="_blank"><i class="fa fa-caret-right" aria-hidden="true"></i>ONGs</a></li>
-                                    <li><a href="<?= URL_AUTHENTIFICATION; ?>"><i class="fa fa-caret-right" aria-hidden="true"></i>1325admin</a></li>
-                                    <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i>Autres liens</a></li>
-                                </ul>
-                            </div>
-                            <!--
-                            <div class="col-lg-6 col-md-6 col-12">
-                                <ul>
-                                    <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i></a></li>
-                                    <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i></a></li>
-                                    <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i></a></li>
-                                    <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i></a></li>
-                                    <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i></a></li>
-                                </ul>
-                            </div>
-                            -->
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-lg-3 col-md-6 col-12">
-                    <div class="single-footer">
-                        <h2>Secrétariat National 1325</h2>
-                        <p>Notre bureau est ouvert à l'adresse suivante : Kinshasa-Gombe, en diagonale du Premier Shopping Mall, dans la concession du Secrétariat au Développement Rural.</p>
-                        <ul class="time-sidual">
-                            <li class="day">Lundi - Vendredi <span>8h00-16h00</span></li>
-                        </ul>
-                    </div>
-                </div>
-
-                <div class="col-lg-3 col-md-6 col-12">
-                    <div class="single-footer">
-                        <h2>Newsletter</h2>
-                        <p>Abonnez-vous à notre newsletter pour recevoir toutes nos actualités dans votre boîte de réception.</p>
-                        <form action="mail/mail.php" method="get" target="_blank" class="newsletter-inner">
-                            <input name="email" placeholder="Adresse mail" class="common-input" onfocus="this.placeholder=''" onblur="this.placeholder='Votre adresse mail'" required type="email">
-                            <button class="button"><i class="icofont icofont-paper-plane"></i></button>
-                        </form>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-    </div>
-    <!--/ End Footer Top -->
-
-    <!-- Copyright -->
-    <div class="copyright">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 col-md-12 col-12">
-                    <div class="copyright-content">
-                        <p>© Copyright 2025 | Tous droits réservés <a href="<?= URL_CONTACT ?>" target="_blank">Secrétariat National Permanent 1325</a></p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!--/ End Copyright -->
-</footer>
-<!--/ End Footer Area -->
-
-<!-- jquery Min JS -->
-<script src="<?= JS_DIR ?>jquery.min.js"></script>
-<!-- jquery Migrate JS -->
-<script src="<?= JS_DIR ?>jquery-migrate-3.0.0.js"></script>
-<!-- jquery Ui JS -->
-<script src="<?= JS_DIR ?>jquery-ui.min.js"></script>
-<!-- Easing JS -->
-<script src="<?= JS_DIR ?>easing.js"></script>
-<!-- Color JS -->
-<script src="<?= JS_DIR ?>colors.js"></script>
-<!-- Popper JS -->
-<script src="<?= JS_DIR ?>popper.min.js"></script>
-<!-- Bootstrap Datepicker JS -->
-<script src="<?= JS_DIR ?>bootstrap-datepicker.js"></script>
-<!-- Jquery Nav JS -->
-<script src="<?= JS_DIR ?>jquery.nav.js"></script>
-<!-- Slicknav JS -->
-<script src="<?= JS_DIR ?>slicknav.min.js"></script>
-<!-- ScrollUp JS -->
-<script src="<?= JS_DIR ?>jquery.scrollUp.min.js"></script>
-<!-- Niceselect JS -->
-<script src="<?= JS_DIR ?>niceselect.js"></script>
-<!-- Tilt Jquery JS -->
-<script src="<?= JS_DIR ?>tilt.jquery.min.js"></script>
-<!-- Owl Carousel JS -->
-<script src="<?= JS_DIR ?>owl-carousel.js"></script>
-<!-- counterup JS -->
-<script src="<?= JS_DIR ?>jquery.counterup.min.js"></script>
-<!-- Steller JS -->
-<script src="<?= JS_DIR ?>steller.js"></script>
-<!-- Wow JS -->
-<script src="<?= JS_DIR ?>wow.min.js"></script>
-<!-- Magnific Popup JS -->
-<script src="<?= JS_DIR ?>jquery.magnific-popup.min.js"></script>
-<!-- Counter Up CDN JS -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/2.0.3/waypoints.min.js"></script>
-<!-- Bootstrap JS -->
-<script src="<?= JS_DIR ?>bootstrap.min.js"></script>
-<!-- Main JS -->
-<script src="<?= JS_DIR ?>main.js"></script>
-</body>
+<!-- Footer Area -->
+
+<footer id="footer" class="footer">
+
+    <!-- Footer Top -->
+
+    <div class="footer-top">
+
+        <div class="container">
+
+            <div class="row">
+
+
+
+                <div class="col-lg-3 col-md-6 col-12">
+
+                    <div class="single-footer">
+
+                        <h2>Suivez-nous</h2>
+
+                        <p>
+
+                            <img src="<?= IMG_DIR ?>logoMingenre03.png" alt="" width="100%" height="100" style="display:block;">
+
+                        </p>
+
+                        <!-- <p>Le Secrétariat National de la Résolution 1325 en RDC, est une structure du suivi et de la gestion quotidienne de la mise en œuvre de ladite Résolution sur l’ensemble du Pays.</p> -->
+
+                        <!-- Social -->
+
+                        <ul class="social">
+
+                            <li><a href="https://web.facebook.com/sn1325/" target="_blank"><i class="icofont-facebook"></i></a></li>
+
+                            <li><a href="https://www.linkedin.com/company/r1325rdc/" target="_blank"><i class="icofont-linkedin"></i></a></li>
+
+                            <li><a href="https://x.com/R1325RDC" target="_blank"><i class="icofont-twitter"></i></a></li>
+
+                            <li><a href="https://youtube.com/@resolution1325rdc?si=IWwQB3N7fc5RFUHD" target="_blank"><i class="icofont-youtube"></i></a></li>
+
+                        </ul>
+
+                        <!-- End Social -->
+
+                    </div>
+
+                </div>
+
+
+
+                <div class="col-lg-3 col-md-6 col-12">
+
+                    <div class="single-footer f-link">
+
+                        <h2>Liens utiles</h2>
+
+                        <div class="row">
+
+                            <div class="col-lg-6 col-md-6 col-12">
+
+                                <ul>
+
+                                    <li><a href="https://genre.gouv.cd/" target="_blank"><i class="fa fa-caret-right" aria-hidden="true"></i>MGFE</a></li>
+
+                                    <li><a href="https://www.unwomen.org/fr" target="_blank"><i class="fa fa-caret-right" aria-hidden="true"></i>ONUFEMMES</a></li>
+
+                                    <li><a href="https://www.norway.no/fr/democratic-republic-of-congo/" target="_blank"><i class="fa fa-caret-right" aria-hidden="true"></i>AMBASSADE NORVEGE</a></li>
+
+                                    <li><a href="#" target="_blank"><i class="fa fa-caret-right" aria-hidden="true"></i>ONGs</a></li>
+
+                                    <li><a href="<?= URL_AUTHENTIFICATION; ?>"><i class="fa fa-caret-right" aria-hidden="true"></i>1325admin</a></li>
+
+                                    <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i>Autres liens</a></li>
+
+                                </ul>
+
+                            </div>
+
+                            <!--
+
+                            <div class="col-lg-6 col-md-6 col-12">
+
+                                <ul>
+
+                                    <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i></a></li>
+
+                                    <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i></a></li>
+
+                                    <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i></a></li>
+
+                                    <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i></a></li>
+
+                                    <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i></a></li>
+
+                                </ul>
+
+                            </div>
+
+                            -->
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+
+
+                <div class="col-lg-3 col-md-6 col-12">
+
+                    <div class="single-footer">
+
+                        <h2>Secrétariat National 1325</h2>
+
+                        <p>Notre bureau est ouvert à l'adresse suivante : Kinshasa-Gombe, en diagonale du Premier Shopping Mall, dans la concession du Secrétariat au Développement Rural.</p>
+
+                        <ul class="time-sidual">
+
+                            <li class="day">Lundi - Vendredi <span>8h00-16h00</span></li>
+
+                        </ul>
+
+                    </div>
+
+                </div>
+
+
+
+                <div class="col-lg-3 col-md-6 col-12">
+
+                    <div class="single-footer">
+
+                        <h2>Newsletter</h2>
+
+                        <p>Abonnez-vous à notre newsletter pour recevoir toutes nos actualités dans votre boîte de réception.</p>
+
+                        <form action="mail/mail.php" method="get" target="_blank" class="newsletter-inner">
+
+                            <input name="email" placeholder="Adresse mail" class="common-input" onfocus="this.placeholder=''" onblur="this.placeholder='Votre adresse mail'" required type="email">
+
+                            <button class="button"><i class="icofont icofont-paper-plane"></i></button>
+
+                        </form>
+
+                    </div>
+
+                </div>
+
+
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <!--/ End Footer Top -->
+
+
+
+    <!-- Copyright -->
+
+    <div class="copyright">
+
+        <div class="container">
+
+            <div class="row">
+
+                <div class="col-lg-12 col-md-12 col-12">
+
+                    <div class="copyright-content">
+
+                        <p>© Copyright 2025 | Tous droits réservés <a href="<?= URL_CONTACT ?>" target="_blank">Secrétariat National Permanent 1325</a></p>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <!--/ End Copyright -->
+
+</footer>
+
+<!--/ End Footer Area -->
+
+
+
+<!-- jquery Min JS -->
+
+<script src="<?= JS_DIR ?>jquery.min.js"></script>
+
+<!-- jquery Migrate JS -->
+
+<script src="<?= JS_DIR ?>jquery-migrate-3.0.0.js"></script>
+
+<!-- jquery Ui JS -->
+
+<script src="<?= JS_DIR ?>jquery-ui.min.js"></script>
+
+<!-- Easing JS -->
+
+<script src="<?= JS_DIR ?>easing.js"></script>
+
+<!-- Color JS -->
+
+<script src="<?= JS_DIR ?>colors.js"></script>
+
+<!-- Popper JS -->
+
+<script src="<?= JS_DIR ?>popper.min.js"></script>
+
+<!-- Bootstrap Datepicker JS -->
+
+<script src="<?= JS_DIR ?>bootstrap-datepicker.js"></script>
+
+<!-- Jquery Nav JS -->
+
+<script src="<?= JS_DIR ?>jquery.nav.js"></script>
+
+<!-- Slicknav JS -->
+
+<script src="<?= JS_DIR ?>slicknav.min.js"></script>
+
+<!-- ScrollUp JS -->
+
+<script src="<?= JS_DIR ?>jquery.scrollUp.min.js"></script>
+
+<!-- Niceselect JS -->
+
+<script src="<?= JS_DIR ?>niceselect.js"></script>
+
+<!-- Tilt Jquery JS -->
+
+<script src="<?= JS_DIR ?>tilt.jquery.min.js"></script>
+
+<!-- Owl Carousel JS -->
+
+<script src="<?= JS_DIR ?>owl-carousel.js"></script>
+
+<!-- counterup JS -->
+
+<script src="<?= JS_DIR ?>jquery.counterup.min.js"></script>
+
+<!-- Steller JS -->
+
+<script src="<?= JS_DIR ?>steller.js"></script>
+
+<!-- Wow JS -->
+
+<script src="<?= JS_DIR ?>wow.min.js"></script>
+
+<!-- Magnific Popup JS -->
+
+<script src="<?= JS_DIR ?>jquery.magnific-popup.min.js"></script>
+
+<!-- Counter Up CDN JS -->
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/2.0.3/waypoints.min.js"></script>
+
+<!-- Bootstrap JS -->
+
+<script src="<?= JS_DIR ?>bootstrap.min.js"></script>
+
+<!-- Main JS -->
+<!-- GMaps JS (doit être chargé avant les scripts qui utilisent `GMaps`) -->
+<script src="<?= JS_DIR ?>gmaps.min.js"></script>
+<script src="<?= JS_DIR ?>map-active.js"></script>
+
+<script src="<?= JS_DIR ?>main.js"></script>
+
+</body>
+
 </html>
