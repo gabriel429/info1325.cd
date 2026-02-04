a6476275b87bfe1fa11614eea4beda5594b05fbe
Gabriel Muya <igabrielmuya@gmail.com>
2026-02-04 17:45:35 +0100

    Renommer la section 'services' en 'axes' (HTML + CSS compatibility)

--- pagesweb/compoAxe.php
+++ pagesweb/compoAxe.php
@@ -1,5 +1,5 @@
-<!-- Start service -->
-		<section class="services section">
+<!-- Start axes -->
+ 		<section class="axes section">
 			<div class="container">
 				<div class="row">
 					<div class="col-lg-12">
@@ -10,53 +10,53 @@
 				</div>
 				<div class="row">
 					<div class="col-lg-4 col-md-6 col-12">
-						<!-- Start Single Service -->
+						<!-- Start Single Axe -->
 						<div class="single-service">
 							<h4><a href="service-details.html">PREVENTION</a></h4>
 							<p> Intégrer la perspective de genre dans la prévention des conflits et éliminer les violences basées sur le genre</p>	
 						</div>
-						<!-- End Single Service -->
+						<!-- End Single Axe -->
 					</div>
 					<div class="col-lg-4 col-md-6 col-12">
-						<!-- Start Single Service -->
+						<!-- Start Single Axe -->
 						<div class="single-service">
 							<h4><a href="service-details.html">PARTICIPATION</a></h4>
 							<p>Garantir la participation pleine et effective des femmes à tous les niveaux de prise de décision</p>	
 						</div>
-						<!-- End Single Service -->
+						<!-- End Single Axe -->
 					</div>
 					<div class="col-lg-4 col-md-6 col-12">
-						<!-- Start Single Service -->
+						<!-- Start Single Axe -->
 						<div class="single-service">
 							<h4><a href="service-details.html">PROTECTION</a></h4>
 							<p>Protéger les femmes et les filles contre toutes les formes de violences dans les conflits</p>	
 						</div>
-						<!-- End Single Service -->
+						<!-- End Single Axe -->
 					</div>
 					<div class="col-lg-4 col-md-6 col-12">
-						<!-- Start Single Service -->
+						<!-- Start Single Axe -->
 						<div class="single-service">
 							<h4><a href="service-details.html">SECOURS ET RELEVEMENT</a></h4>
 							<p>Assurer l'accès aux services de base et soutenir le relèvement économique des femmes affectées par les conflits</p>	
 						</div>
-						<!-- End Single Service -->
+						<!-- End Single Axe -->
 					</div>
 					<div class="col-lg-4 col-md-6 col-12">
-						<!-- Start Single Service -->
+						<!-- Start Single Axe -->
 						<div class="single-service">
 							<h4><a href="service-details.html">GESTION DES CONFLITS ÉMERGENTS ET L’AIDE HUMANITAIRE</a></h4>
 						</div>
-						<!-- End Single Service -->
+						<!-- End Single Axe -->
 					</div>
 					<div class="col-lg-4 col-md-6 col-12">
-						 <!-- Start Single Service -->
+						 <!-- Start Single Axe -->
 						<div class="single-service">
 							<h4><a href="service-details.html"></a></h4>
 							<p>Ce cadre stratégique permet à la RDC de traduire en actions concrètes les engagements internationaux pour les droits des femmes dans les contextes de conflit et post-conflit.</p>	
 						</div>
-						<!-- End Single Service -->
+						<!-- End Single Axe -->
 					</div>
 				</div>
 			</div>
 		</section>
-		<!--/ End service -->
+		<!--/ End axes -->
